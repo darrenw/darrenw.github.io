717211b912404b45fcc901983e5eae899abdc4d6
Darren W <darren.willis@gmail.com>
2021-12-15 14:12:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
+  setTimeout(() => { e.prompt();}, 5000);
   // Update UI to notify the user they can add to home screen
   addBtn.style.display = 'block';
 
