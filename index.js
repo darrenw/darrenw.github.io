cc2dc0ce02e65bbe0212b9ff6e053539b4108887
Darren W <darren.willis@gmail.com>
2021-12-15 14:26:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -31,9 +31,6 @@ window.addEventListener('beforeinstallprompt', (e) => {
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
-  console.log("Setting timeout");
-  setTimeout(() => { e.prompt(); console.log("timeout called");}, 5000);
-  console.log("timeout set");
   // Update UI to notify the user they can add to home screen
   addBtn.style.display = 'block';
 
@@ -41,7 +38,11 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // hide our user interface that shows our A2HS button
     addBtn.style.display = 'none';
     // Show the prompt
-    deferredPrompt.prompt();
+      console.log("Setting timeout");
+  setTimeout(() => { e.prompt(); console.log("timeout called");}, 5000);
+  console.log("timeout set");
+
+//     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
